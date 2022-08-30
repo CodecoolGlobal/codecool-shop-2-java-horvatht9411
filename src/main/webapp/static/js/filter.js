b1675c07164fe130c92910fcb5d7c7441b2f9023
horvatht9411 <horvatht9411@gmail.com>
2022-08-30 14:47:30 +0200

    feat: add eventlisener to categories and supliers

---
+++ src/main/webapp/static/js/filter.js
@@ -0,0 +1,11 @@
+document.querySelector("#category").addEventListener("change", evt => {
+    console.log(evt.target.value);
+} );
+
+
+document.querySelector("#supplier").addEventListener("change", evt => {
+    console.log(evt.target.value);
+})
+s
+
+
