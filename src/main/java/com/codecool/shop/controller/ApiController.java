9d62cd93f9daa28f11f27875a3d4947c52c53dca
Hladgerd <hladgerd@gmail.com>
2022-08-31 08:46:31 +0200

    feat: implement add to cart

---
+++ src/main/java/com/codecool/shop/controller/ApiController.java
@@ -0,0 +1,61 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = {"/api/addToCart", "/api/removeFromCart", "/api/deleteCart", "/api/editCart"})
+public class ApiController extends HttpServlet {
+    private final Map<Product, Integer> cart = new HashMap<>();
+    private final ProductDao productDataStore = ProductDaoMem.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        HttpSession session = request.getSession();
+        String servletPath = request.getServletPath();
+
+        switch (servletPath) {
+
+            case "/api/addToCart":
+                int productId = Integer.parseInt(request.getParameter("id"));
+                Product product = productDataStore.find(productId);
+                cart.put(product, cart.getOrDefault(product, 0) + 1);
+                session.setAttribute("cart", cart);
+
+                int quantity = cart.values().stream().reduce(0, Integer::sum);
+                String json = new Gson().toJson(quantity);
+                out.println(json);
+                break;
+
+            case "/api/removeFromCart":
+//                TODO complete
+                break;
+
+            case "/api/deleteCart":
+//                TODO complete case
+                break;
+
+            case "/api/editCart":
+//                TODO case
+                break;
+
+        }
+        out.flush();
+    }
+}
