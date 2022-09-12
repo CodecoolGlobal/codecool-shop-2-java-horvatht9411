a630f9e3f8332a72292d557d9022b722cf2c2496
Hladgerd <hladgerd@gmail.com>
2022-09-12 13:32:57 +0200

    add doPut and DoDelete methods in order to have CRUD controls under one class

--- src/main/java/com/codecool/shop/controller/AddCartController.java
+++ src/main/java/com/codecool/shop/controller/AddCartController.java
@@ -12,11 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
-@WebServlet(urlPatterns = {"/api/addToCart/*", "/api/cartQty"})
+@WebServlet(urlPatterns = {"/api/addToCart/*", "/api/cartQty", "/api/editCart/*", "/api/removeFromCart/*"})
 public class AddCartController extends HttpServlet {
     private Map<Product, Integer> cart = new HashMap<>();
     private final ProductDao productDataStore = ProductDaoMem.getInstance();
@@ -38,6 +41,7 @@ public class AddCartController extends HttpServlet {
         if (session.getAttribute("cart") != null) {
             cart = (Map<Product, Integer>) session.getAttribute("cart");
         }
+
         int productId = ControllerUtil.retrieveProductId(request);
         Product product = productDataStore.find(productId);
         cart.put(product, cart.getOrDefault(product, 0) + 1);
@@ -47,6 +51,65 @@ public class AddCartController extends HttpServlet {
         String json = new Gson().toJson(quantity);
         out.println(json);
         out.flush();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = ControllerUtil.initResponse(response);
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("cart") != null) {
+            cart = (Map<Product, Integer>) session.getAttribute("cart");
+        }
+
+        int productId = ControllerUtil.retrieveProductId(request);
+        Product updatedProduct = cart.keySet()
+                .stream()
+                .filter(product -> product.getId() == productId).findFirst().orElse(null);
+        String updatedQuantity = ControllerUtil.inputStreamToString(request.getInputStream());
+        cart.put(updatedProduct, Integer.valueOf(updatedQuantity));
+
+        session.setAttribute("cart", cart);
+
+        assert updatedProduct != null;
+        int itemTotal = calculateItemTotal(updatedProduct);
+        int bigTotal = calculateBigTotal();
+        int[] totals = {itemTotal, bigTotal};
+        String json = new Gson().toJson(totals);
+        out.println(json);
+        out.flush();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = ControllerUtil.initResponse(response);
+        HttpSession session = request.getSession();
+        if (session.getAttribute("cart") != null) {
+            cart = (Map<Product, Integer>) session.getAttribute("cart");
+        }
+        int productId = ControllerUtil.retrieveProductId(request);
+        Product deletedProduct = cart.keySet()
+                .stream()
+                .filter(item -> item.getId() == productId).findFirst().orElse(null);
+        cart.remove(deletedProduct);
+        session.setAttribute("cart", cart);
+        int bigTotal = calculateBigTotal();
+        String data = new Gson().toJson(bigTotal);
+        out.println(data);
+        out.flush();
+    }
+
+    private int calculateItemTotal(Product product) {
+        return product.getDefaultPrice() * cart.get(product);
+    }
+
+    private int calculateBigTotal() {
+        int bigTotal = 0;
+        for (Product product : cart.keySet()) {
+            int itemTotal = calculateItemTotal(product);
+            bigTotal += itemTotal;
+        }
+        return bigTotal;
     }
 
     private int getCartQty() {
