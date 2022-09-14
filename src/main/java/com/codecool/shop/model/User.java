270b9dc7556d113d77e49a46b91add41730eb71b
fergencszeno <fergencszeno@gmail.com>
2022-09-14 12:05:38 +0200

    feat: add getters

--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -2,11 +2,25 @@ package com.codecool.shop.model;
 
 public class User extends BaseModel{
     private final String email;
-    private final String password;
+    private final byte[] hashedPassword;
+    private final byte[] salt;
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, byte[] hashedPassword, byte[] salt) {
         super(name);
         this.email = email;
-        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.salt = salt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public byte[] getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public byte[] getSalt() {
+        return salt;
     }
 }
