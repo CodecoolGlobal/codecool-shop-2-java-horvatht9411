9db6bb82f67f604cde6da5a2fd7207b7838eb45d
fergencszeno <fergencszeno@gmail.com>
2022-09-14 12:05:26 +0200

    feat: save user into db

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.PasswordHashing;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.User;
 import com.codecool.shop.service.LogService;
+import com.codecool.shop.service.UserService;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -13,9 +16,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 @WebServlet(urlPatterns = {"/registration"})
 public class RegistrationController extends HttpServlet {
+
+    private final UserService userService = new UserService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -35,6 +43,16 @@ public class RegistrationController extends HttpServlet {
         String name = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        try {
+            byte[] salt = PasswordHashing.generateSalt();
+            byte[] hashedPassword = PasswordHashing.hashPassword(password, salt);
+            User newUser = new User(name, email, hashedPassword, salt);
+            userService.addNewUser(newUser);
+            System.out.println(Arrays.toString(salt));
+            System.out.println(Arrays.toString(hashedPassword));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
         response.sendRedirect("/login");
     }
 }
