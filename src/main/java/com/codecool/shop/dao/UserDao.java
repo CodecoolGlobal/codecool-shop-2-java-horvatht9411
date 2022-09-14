798ff0a82b71c69029fe075e50c1d7ef7c7ba48e
fergencszeno <fergencszeno@gmail.com>
2022-09-14 12:06:09 +0200

    feat: implement UserDao interface

--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -1,4 +1,13 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.User;
+
+import java.util.Set;
+
 public interface UserDao {
+    Set<User> getAll();
+
+    User getByEmail(String email);
+
+    void add(User user);
 }
