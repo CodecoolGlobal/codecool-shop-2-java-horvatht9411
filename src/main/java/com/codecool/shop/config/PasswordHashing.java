a201403bade10017b322680e6fb5b8df31161201
Hladgerd <hladgerd@gmail.com>
2022-09-14 08:40:44 +0200

    feat: implement password hashing

---
+++ src/main/java/com/codecool/shop/config/PasswordHashing.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.config;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class PasswordHashing {
+
+    private static byte[] generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public static byte[] hashPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] salt = generateSalt();
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return hash;
+    }
+}
