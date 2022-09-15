1b0d147e20d8e2a92648877818c6a713be944387
horvatht9411 <horvatht9411@gmail.com>
2022-09-15 14:28:11 +0200

    fix: showing register and login

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -1,9 +1,13 @@
 const searchBar = document.querySelector("#search-input");
 const cartQty = document.querySelector(".cart-qty");
+const login = document.querySelector("#login")
+const register = document.querySelector("#register")
 
 function hideSearchBar() {
     searchBar.parentElement.textContent = '';
     cartQty.parentElement.textContent = '';
+    login.textContent = "";
+    register.textContent = "";
 }
 
 hideSearchBar();
